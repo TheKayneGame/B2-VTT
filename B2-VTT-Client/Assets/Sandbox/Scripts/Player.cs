9113b8bfbdca062ae80f56f329410d03ba11eb53
giquon <giquon.gamedev@gmail.com>
2021-03-25 22:45:14 +0100

    Experimented with networking

---
+++ B2-VTT-Client/Assets/Sandbox/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    public override void OnStartLocalPlayer()
+	{
+		Camera.main.transform.SetParent(transform);
+		Camera.main.transform.localPosition	= transform.position;
+		Camera.main.transform.rotation		= transform.rotation;
+	}
+}
