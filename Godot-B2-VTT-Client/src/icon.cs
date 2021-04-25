8f588c3b9b4b42b9fc830a952b03652a1d9795e2
Kayne <kayne.saridjo@gmail.com>
2021-04-25 16:26:09 +0200

    Created Token Prefab Started Debug UI Prefab Working Drag And Drop System

--- Godot-B2-VTT-Client/src/icon.cs
+++ Godot-B2-VTT-Client/src/icon.cs
@@ -4,6 +4,8 @@ using System;
 public class icon : Area2D
 {
     // Declare member variables here. Examples:
+    [Export]
+    public string DragGroup = "";
     private bool _dragging = false;
     private bool _rotating = false;
     private Vector2 clickPos;
@@ -32,46 +34,81 @@ public class icon : Area2D
         if (_rotating)
         {
             float angle = Position.AngleToPoint(GetViewport().GetMousePosition());
-            fovCone.RotationDegrees = (float) Math.Round(((180 / Math.PI) * angle)/90) * 90;
+            fovCone.RotationDegrees = (float)Math.Round(((180 / Math.PI) * angle) / 90) * 90;
             GD.Print(angle);
         }
-            
+
     }
 
-    public void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx){
+    public void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx)
+    {
         //GD.Print("event?");
         Vector2 mousePos = (@event as InputEventMouse).Position;
-
-        if (@event.IsActionPressed("token_interact_primary"))
+        if (!GetTree().IsInputHandled() && IsOnTop())
         {
-            clickPos = Position - mousePos;
-            _dragging = true;
-            GD.Print("Dragging Location: ", clickPos);
-        }
+            
 
-        if (@event.IsActionPressed("token_interact_secondary"))
-        {
-            curMousePos = mousePos;
+            if (@event.IsActionPressed("token_interact_primary"))
+            {
+                clickPos = Position - mousePos;
+                _dragging = true;
+                GD.Print("Dragging Location: ", clickPos);
+                GetTree().SetInputAsHandled();
+                Raise();
+            }
+
+            if (@event.IsActionPressed("token_interact_secondary"))
+            {
+                curMousePos = mousePos;
                 _rotating = true;
+                fovCone.Visible = true;
                 GD.Print("Rotating", curMousePos);
-        }
-    }
+                GetTree().SetInputAsHandled();
+                Raise();
+            }
 
-    public void _MouseEnter(){
-        GD.Print("Enter!");
+        }
+
 
     }
     public override void _Input(InputEvent @event)
     {
-        if (@event.IsActionReleased("token_interact_secondary"))
-        {
-            _rotating = false;
-        }
+
         if (@event.IsActionReleased("token_interact_primary"))
         {
             GD.Print("Dropped");
             _dragging = false;
         }
+
+        if (@event.IsActionReleased("token_interact_secondary"))
+        {
+            _rotating = false;
+            fovCone.Visible = false;
+        }
+        //GetTree().SetInputAsHandled();
+    }
+
+    public void _MouseEnter()
+    {
+        AddToGroup("hovering");
+        GD.Print("Enter!");
+
+    }
+
+    public void _MouseExit()
+    {
+
+        RemoveFromGroup("hovering");
+        GD.Print("Exited!");
+
+    }
+
+
+    private bool IsOnTop()
+    {
+        Godot.Collections.Array hoveringNodes  = GetTree().GetNodesInGroup("hovering");
+        Node2D topNode = hoveringNodes[hoveringNodes.Count - 1] as Node2D;
+        return object.ReferenceEquals(topNode, this);
     }
 
 
